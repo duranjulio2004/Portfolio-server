fbadcfcbcd85dea674c6bd8412017dcc66577e8b
Julio Duran <duranjulio2004@gmail.com>
2025-07-15 10:13:14 -0400

    Inittial Commit

---
+++ server.js
@@ -0,0 +1,48 @@
+import OpenAI from "openai";
+import cors from "cors";
+import express from "express";
+
+// Initial app setup
+const client = new OpenAI();
+const app = express();
+const corsOptions = {
+    origin: ["http://localhost:5173"],
+};
+
+app.use(cors(corsOptions))
+
+// Wasnt really using this, only using post right now for the chatbot
+// let message = "Initial message";
+
+// app.get("/", (req, res) => {
+//     res.json((message));
+// });
+
+app.post("/", express.json(), async (req,res) => {
+    const { newMessage } = req.body;
+
+    client.chat.completions
+    .create({
+      model: "gpt-3.5-turbo",
+      messages: [
+        { role: "user", content: newMessage },
+        { role: "system", content: "You are an assistant chatbot in a portfolio website created by a Software Developer called Julio Duran, give short and positive responses. If you dont know something make it up."}
+        ],
+    })
+    .then((response) => {
+      res.json({ reply: response.choices[0].message.content });
+    })
+    .catch((err) => {
+      console.error("OpenAI error:", err);
+      res.status(500).json({ error: "OpenAI request failed" });
+    });
+
+})
+
+app.listen(3000, () => {
+    console.log("Server started on port 3000");
+});
+
+
+
+// console.log(response.output_text);
